b09eab74c1a76da5930e8e6c5268578f3137b102
Héctor Sebastián Trejo Luna <32781378+hstrejoluna@users.noreply.github.com>
2019-08-15 22:23:03 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import imagen from "./cryptomonedas.png";
+import Cotizacion from "./components/Cotizacion";
+import Formulario from "./components/Formulario";
+import Spinner from "./components/Spinner";
+
+function App() {
+  const [moneda, guardarMoneda] = useState("");
+  const [criptomoneda, guardarCriptomoneda] = useState("");
+  const [cargando, guardarCargando] = useState(false);
+  const [resultado, guardarResultado] = useState({});
+
+  useEffect(() => {
+    const cotizarCriptomoneda = async () => {
+      // si no hay moneda, no ejecutar
+      if (moneda === "") return;
+      const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+      const resultado = await axios.get(url);
+
+      // mostrar spinner
+      guardarCargando(true);
+
+      // ocultar spinner y agregar el resultado
+      setTimeout(() => {
+        guardarCargando(false);
+        guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda]);
+      }, 3000);
+    };
+
+    cotizarCriptomoneda();
+  }, [criptomoneda, moneda]);
+
+  // mostrar Spinner o resultado
+  const componente = cargando ? (
+    <Spinner />
+  ) : (
+    <Cotizacion resultado={resultado} />
+  );
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="one-half column">
+          <img src={imagen} alt="imagen criptomonedas" className="logotipo" />
+        </div>
+        <div className="one-half column">
+          <h1>Cotiza Criptomonedas al Instante</h1>
+
+          <Formulario
+            guardarMoneda={guardarMoneda}
+            guardarCriptomoneda={guardarCriptomoneda}
+          />
+          {componente}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
